1f473aeaab1e56dea13ef8d85d7cb9945392ad65
Lev-Dolzhenko <WorkSpace2707@yandex.ru>
2023-10-24 18:12:17 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+var movableDiv = document.getElementById("movableDiv");
+
+      var isDragging = false;
+      var startX = 0, startY = 0;
+
+      movableDiv.addEventListener("mousedown", function(event) {
+        isDragging = true;
+        startX = event.clientX - movableDiv.offsetLeft;
+        startY = event.clientY - movableDiv.offsetTop;
+      });
+
+      document.addEventListener("mouseup", function() {
+        isDragging = false;
+      });
+
+      document.addEventListener("mousemove", function(event) {
+        event.preventDefault();
+        if (!isDragging) return;
+        movableDiv.style.left = event.clientX - startX + 'px';
+        movableDiv.style.top = event.clientY - startY + 'px';
+      });
+
+      
